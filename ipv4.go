075998ff0055334cee861669e4da5c30cbf4336a
Adam Barrett <adam.barrett.ext@bayer.com>
2020-09-24 11:53:18 -0600

    moved files

--- ipv4.go
+++
@@ -1,8 +0,0 @@
-package ipv4
-
-import "regexp"
-
-func Validate(address string) bool {
-	re := regexp.MustCompile(`^(([0-9]{1}|[0-9]{2}|1[0-9]{2}|2([0-4][0-9]|5[0-5])).){3}([0-9]{1}|[0-9]{2}|1[0-9]{2}|2([0-4][0-9]|5[0-5]))($|\/([0-9]{1}|([1-2][0-9]|3[0-2]){1,2})$)`)
-	return re.Match([]byte(address))
-}
